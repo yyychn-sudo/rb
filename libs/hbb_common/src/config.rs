f0029fe46d90dc894203ead579dcc70b945be664
yyychn-sudo <107602361@qq.com>
2026-07-17 09:27:46 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -117,8 +117,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["106.15.251.61"];
-pub const RS_PUB_KEY: &str = "E4FQ579RywArvAgYt1JZCmb8ZkkD7VRxApIoa4ImN9k=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["123.45.67.89"];
+pub const RS_PUB_KEY: &str = "E4FQ579SywArvAgYt1JZAmb8ZkkD7VRxApIoa4ImN9k=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
